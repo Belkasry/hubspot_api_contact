15b8bf646681acb1e674a0b597b29d65268f2283
oualid.belkasri <oualid.belkasri@tersea.com>
2024-03-26 18:02:31 +0000

    cors 2

--- app.js
+++ app.js
@@ -4,8 +4,15 @@ var cors = require('cors')
 const app = express();
 app.use(express.json());
 app.use(cors())
+//allow all requests from all domains & subdomains
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 require('dotenv').config()
-app.options('*', cors())
 const hubspot = require('@hubspot/api-client')
 const hubspotClient = new hubspot.Client({accessToken: process.env.HUBSPOT_API_KEY});
 const router = express.Router();
